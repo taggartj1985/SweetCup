707069685abfea6e1b444ab4f26f307c902626b1
taggartj1985 <taggartj1985@gmail.com>
2020-09-05 12:25:30 +0100

    added styling to button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,12 +12,13 @@ import Default from './components/Default';
 function App() {
   return (
     <React.Fragment>
-      <h3>hello from app</h3>
       <Navbar/>
-      <ProductList/>
-      <Details/>
-      <Cart/>
-      <Default/>
+      <Switch>
+        <Route exact path="/" component={ProductList}/>
+        <Route path="/details" component={Details}/>
+        <Route path="/cart" component={Cart}/>
+        <Route component={Default}/>
+      </Switch>
     </React.Fragment>
   );
 }
