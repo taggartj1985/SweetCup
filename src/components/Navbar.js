d899943d9c2cc53a14b200675e869ecb6fc88826
taggartj1985 <taggartj1985@gmail.com>
2020-09-08 11:51:03 +0100

    products showing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,8 @@ import {ButtonContainer} from './Button';
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
-            {/* 
-https://www.iconfinder.com/icons/1243689/call_phone_icon
-Creative Commons (Attribution 3.0 Unported);
-        https://www.iconfinder.com/Makoto_msk */}
+      <NavWarpper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+           
           <Link to='/'>
             <img src={logo} alt="logo"
             className="navbar-brand"/>
@@ -31,7 +28,17 @@ Creative Commons (Attribution 3.0 Unported);
               </span>
             </ButtonContainer>
           </Link>
-      </nav>
+      </NavWarpper>
     )
   }
 }
+
+const NavWarpper = styled.nav`
+background:var(--mainBlue);
+.nav-link{
+  color:var(--mainWhite)!important;
+  font-size:1.3rem;
+  text-transform: capitalize;
+}
+
+`
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import {ProductProvider} from './context';
 
 ReactDOM.render(
+  <ProductProvider>
   <Router>
     <App />
-  </Router>,
+  </Router>
+  </ProductProvider>,
   document.getElementById('root')
 );
 
